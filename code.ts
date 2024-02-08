9eb9f5f51c1b780bff8ae9d107a05ad9385bc482
Purujit Saha <purujit.saha@gmail.com>
2024-02-07 20:52:18 -0800

    Fix chart size issues

--- code.ts
+++ code.ts
@@ -1,12 +1,16 @@
 function onOpen() {
-    SpreadsheetApp.getUi()
-        .createMenu('Cash Flow')
+    var ui = SpreadsheetApp.getUi();
+    ui.createMenu('Cash Flow')
         .addItem('Cash Flow Sankey', 'userActionShowCashFlowChart')
         .addToUi();
 }
 
 function userActionShowCashFlowChart() {
     const template = HtmlService.createTemplateFromFile('sankey_chart_ui');
-    SpreadsheetApp.getUi().showModalDialog(template.evaluate().setWidth(600).setHeight(1350)
+    // Google sheets gives us no way of knowing how big the screen is. Otherwise, we could
+    // have used the total screen size to compute a reasonable size for the dialog.
+    // This 640x640 should work for most desktop screens.
+    // See https://stackoverflow.com/questions/25293568/how-to-determine-google-sheets-window-size
+    SpreadsheetApp.getUi().showModalDialog(template.evaluate().setWidth(640).setHeight(640)
         , 'Cash Flow');
 }
